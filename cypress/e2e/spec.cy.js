052f6bfcc7ddb3f436d57015a8dbbf8dfdcd2197
Max Lange <max@langes.us>
2023-02-21 13:17:25 -0500

    Add beforeEach

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
-describe('empty spec', () => {
+describe('Dashboard Spec', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/reservations', {fixture: 'reservations.json'})
+    cy.visit('http://localhost:3000/')
+  })
+  
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    
   })
 })
