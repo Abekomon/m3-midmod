7cddc7c04f425ae03a29be13d56fb754baa36823
Max Lange <max@langes.us>
2023-02-21 12:45:59 -0500

    Update App to include form, and start stateful Form buildout

--- src/App/App.js
+++ src/App/App.js
@@ -18,15 +18,22 @@ class App extends Component {
       this.setState({ resData: data, isLoading: false })
     })
   }
+
+  submitRes = (reservation) => {
+    reservation["id"] = this.state.resData.length + 1
+    this.setState({ resData: [...this.state.resData, reservation] })
+  }
   
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
-        {/* <div className='resy-form'></div> */}
+        <div className='resy-form'>
+          <Form submitRes={this.submitRes} />
+        </div>
         {
-          this.state.isLoading ? <h2>Loading...</h2> :
-          !this.state.resData.length ? <h2>No current reservations</h2> :
+          this.state.isLoading ? <h2 className='loading-text'>Loading...</h2> :
+          !this.state.resData.length ? <h2 className='info-text'>No current reservations</h2> :
           <div className='resy-container'>
             {this.state.resData.map(res => 
               <Reservation 
--- src/Form/Form.js
+++ src/Form/Form.js
@@ -8,14 +8,57 @@ class Form extends Component {
       name: '',
       date: '',
       time: '',
-      guests: 0
+      guests: null,
     }
+  }
 
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
-    
+    return (
+      <form>
+        <input 
+          type='text'
+          name='name'
+          value={this.state.name}
+          placeholder='Name'
+          onChange={(e) => this.handleChange(e)}
+        />
+        <input 
+          type='text'
+          name='date'
+          value={this.state.date}
+          placeholder='Date (mm/dd)'
+          onChange={(e) => this.handleChange(e)}
+        />
+        <input 
+          type='text'
+          name='time'
+          value={this.state.time}
+          placeholder='Time (h:mm)'
+          onChange={(e) => this.handleChange(e)}
+        />
+        <input 
+          type='number'
+          name='guests'
+          min='1'
+          value={this.state.guests}
+          placeholder='Number of guests'
+          onChange={(e) => this.handleChange(e)}
+        />
+        <button onClick={(e) => {
+          e.preventDefault()
+          const newRes = {
+            id: Date.now()
+          }
+          this.props.submitRes({})
+        }}>Make Reservation</button>
+      </form>
+    )
   }
+
 }
 
 export default Form
