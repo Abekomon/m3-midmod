877f02b2ccd78d2ede81e3410d7f5fd85b78fd6a
Max Lange <max@langes.us>
2023-02-21 11:55:21 -0500

    Update App to call data, and conditionally render based on state

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Form from '../Form/Form';
+import Reservation from '../Reservation/Reservation';
+import accessReservationData from '../apiCalls';
 
 class App extends Component {
   constructor() {
@@ -12,7 +14,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    
+    accessReservationData("reservations").then(data => {
+      this.setState({ resData: data, isLoading: false })
+    })
   }
   
   render() {
@@ -20,9 +24,22 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         {/* <div className='resy-form'></div> */}
-        <div className='resy-container'>
-          
-        </div>
+        {
+          this.state.isLoading ? <h2>Loading...</h2> :
+          !this.state.resData.length ? <h2>No current reservations</h2> :
+          <div className='resy-container'>
+            {this.state.resData.map(res => 
+              <Reservation 
+                key={res.id}
+                id={res.id}
+                date={res.date}
+                name={res.name}
+                time={res.time}
+                guests={res.number}
+              />
+            )}
+          </div> 
+        }
       </div>
     )
   }
