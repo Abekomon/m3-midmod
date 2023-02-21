54da0cf2898827e6e776175e36b7be75e8f30669
Max Lange <max@langes.us>
2023-02-21 11:29:08 -0500

    Add file setup for Form component

---
+++ src/Form/Form.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react"
+import "./Form.css"
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      date: '',
+      time: '',
+      guests: 0
+    }
+
+  }
+
+  render() {
+    
+  }
+}
+
+export default Form
